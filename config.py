0b8bb1187f846b2adfa2f9d713a0270ae615b120
sj-qi <shrajal.jain@qi-cap.com>
2025-04-01 12:31:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,14 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
+import streamlit as st
 
 # API Keys for LLM
-API_KEY = os.getenv("ANTHROPIC_API_KEY")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+API_KEY = st.secrets("ANTHROPIC_API_KEY")
+OPENAI_API_KEY = st.secrets("OPENAI_API_KEY")
 
 # Database Credentials
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = st.secrets("DB_HOST")
+DB_USER = st.secrets("DB_USER")
+DB_NAME = st.secrets("DB_NAME")
+DB_PASSWORD = st.secrets("DB_PASSWORD")
 
 # Generic Variables
-CSV_FILE_PATH = os.getenv("CSV_FILE_PATH")
+CSV_FILE_PATH = st.secrets("CSV_FILE_PATH")
