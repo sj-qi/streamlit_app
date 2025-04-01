5571075bb56fa2327c7015a66c4120190fab5b6e
sj-qi <shrajal.jain@qi-cap.com>
2025-04-01 12:45:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 # API Keys for LLM
-API_KEY = st.secrets("ANTHROPIC_API_KEY")
-OPENAI_API_KEY = st.secrets("OPENAI_API_KEY")
+API_KEY = st.secrets["ANTHROPIC_API_KEY"]
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 # Database Credentials
-DB_HOST = st.secrets("DB_HOST")
-DB_USER = st.secrets("DB_USER")
-DB_NAME = st.secrets("DB_NAME")
-DB_PASSWORD = st.secrets("DB_PASSWORD")
+DB_HOST = st.secrets["DB_HOST"]
+DB_USER = st.secrets["DB_USER"]
+DB_NAME = st.secrets["DB_NAME"]
+DB_PASSWORD = st.secrets["DB_PASSWORD"]
 
 # Generic Variables
-CSV_FILE_PATH = st.secrets("CSV_FILE_PATH")
+CSV_FILE_PATH = st.secrets["CSV_FILE_PATH"]
